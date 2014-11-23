99630df08a91f576cb725a60ee89d2f724d6552e
TheChyz <thechyz@hotmail.com>
2014-11-22 23:36:11 -0800

    Update dataTypes.cpp

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -4,8 +4,9 @@ namespace dataTypes {
 
 	vector<string> tableOfUserTypes(0);
 	int tableOfSetsUserTypesSize = sizeof(tableOfSetsUserTypes) / sizeof(tableOfSetsUserTypes[0]);
-
 	int tableOfTypesSize = sizeof(tableOfTypes) / sizeof(tableOfTypes[0]);
+
+
 
 	//returns true if the given string is a data type of c++ or user defined data type, false otherwise
 	bool isDataType(const string& stringRep) {
@@ -23,6 +24,8 @@ namespace dataTypes {
 	}
 
 
+
+
 	bool isSetsUserType(const string& stringRep) {
 		for (int i = 0; i < tableOfSetsUserTypesSize; i++) {
 			if (stringRep == tableOfSetsUserTypes[i]) {
@@ -32,10 +35,49 @@ namespace dataTypes {
 		return false;
 	}
 
+
+
 	Token* setDataTypeList(Token* firstToken) {
 		TokenList* dataTypeList = new TokenList;
-		return NULL;
+		Token* currentToken;
+		currentToken = firstToken;
+		string storedDataType = "";
+		bool found;
+		while (currentToken) {
+			found = false;
+			while (currentToken->getNext() && !found) {
+				if (isDataType(currentToken->getStringRep())) {
+					storedDataType = currentToken->getStringRep();
+					currentToken = currentToken->getNext();
+				}
+				else {
+					if ((currentToken->getNext()->getStringRep().find('{') != -1) || (currentToken->getNext()->getStringRep().find('(') != -1)) {
+						currentToken = currentToken->getNext();
+						storedDataType = "";
+						break;
+					}
+					else if ((currentToken->getStringType() == ensc251::T_Identifier) && (!storedDataType.empty())) {
+						if ((currentToken->getNext() != NULL) && ((currentToken->getNext()->getStringRep().find('=') != -1) || (currentToken->getNext()->getStringRep().find(';') != -1))) {
+							Token* temp = new Token;
+							temp->setDataType(storedDataType);
+							temp->setStringRep(currentToken->getStringRep());
+							dataTypeList->append(temp);
+							storedDataType = "";
+							found = true;
+							break;
+						}
+						currentToken = currentToken->getNext();
+					}
+					else {
+						currentToken = currentToken->getNext();
+					}
+				}
+			}
+			currentToken = currentToken->getNext();
+		}
+		return dataTypeList->getFirst();
 	}
+
 
 
 	void setsUserDataTypes(Token* firstToken) {
@@ -46,7 +88,14 @@ namespace dataTypes {
 				while (currentToken->getNext()) {
 					if ((currentToken->getNext()->getStringRep().find(';') != -1) || (currentToken->getNext()->getStringRep().find('=') != -1) ||
 						(currentToken->getNext()->getStringRep().find('{') != -1)) {
-						tableOfUserTypes.push_back(currentToken->getStringRep());
+						
+						//if table of user types contains the string in current token, don't add it (aka do nothing), otherwise add it
+						if (std::find(tableOfUserTypes.begin(), tableOfUserTypes.end(), currentToken->getStringRep()) != tableOfUserTypes.end()) {
+							//DO NOTHING
+						}
+						else {
+							tableOfUserTypes.push_back(currentToken->getStringRep());
+						}
 						break;
 					}
 					else {
